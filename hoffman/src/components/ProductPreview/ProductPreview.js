5515269ce7d5e49fee7c1d75fc2ddc11423a90a0
Adam Hoffman <wintros@yahoo.com>
2020-03-24 18:37:57 -0600

    preview page is up, time for product display page

--- hoffman/src/components/ProductPreview/ProductPreview.js
+++ hoffman/src/components/ProductPreview/ProductPreview.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
-import { setLocale } from 'yup'
+import './ProductPreview.css'
+import { Link } from 'react-router-dom'
 
 const ProductPreview = () => {
     const [product, setProduct] = useState([])
@@ -9,25 +10,14 @@ const ProductPreview = () => {
     useEffect(() => {
         axios.get('http://localhost:5000/api/merchandise')
         .then(res => {
-            setProduct(res.data)           
+            setProduct(res.data)       
         })
-    }, [])
-
-    console.log(product)
+    }, [])    
     
-
-    console.log('ALL', all)
-
     const id = product.slice(-1).map(p => {return p.id})
-    console.log("ID", id)
-    const newId = id[0] 
-    console.log('NEWID', newId)  
-   
-
-    
+    const newId = id[0]        
 
     useEffect(() => {       
-       
         axios.get(`http://localhost:5000/api/merchandise/${newId}`)
         .then(res => {
             setAll(res.data)
@@ -35,20 +25,24 @@ const ProductPreview = () => {
         })
     }, [product])
 
+    const Delete = () => {
+        axios.delete(`http://localhost:5000/api/merchandise/${newId}`)
+        .then(res => {
+            console.log(res)
+            console.log(window)
+            window.alert("You've deleted this product. Forever. It's not coming back. Good job. Get yourself a cookie")            
+        })
+    }
+
     console.log("PRODUCT", product.slice(-1)[0])
     return (
-        <div>
-            <img src={`http://res.cloudinary.com/hoffman-house/image/upload/${all.image}.jpg`} />
-            {product.slice(-1).map(p => {
-                return <div>
-                    <img src={`http://res.cloudinary.com/hoffman-house/image/upload/${p.image}.jpg`} />
-                    <p>{p.category}</p>
-                    <p>{p.description}</p>
-                    <p>{p.price}</p>
-                    <p>{p.quanity}</p>
-                    <p>{p.weight}</p>
-                </div>
-            })}
+        <div className="image_container">
+            <img className="product_image_preview" src={`http://res.cloudinary.com/hoffman-house/image/upload/${all.image}.jpg`} />
+            <div className='text_continaer'>
+            <p>Description: {all.description}</p>
+            <p>Price: ${all.price}</p>
+            </div>
+            <button onClick={Delete}><Link to="/products">Delete</Link></button>
         </div>
     )
 }
