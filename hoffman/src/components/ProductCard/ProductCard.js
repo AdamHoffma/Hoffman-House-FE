c22d1c8f409f8e0be8c65d37aa8dc166c0c0c811
Adam Hoffman <wintros@yahoo.com>
2020-03-27 12:45:35 -0600

    product card

---
+++ hoffman/src/components/ProductCard/ProductCard.js
@@ -0,0 +1,11 @@
+import React, { useState, useEffect} from 'react'
+import './ProductCard.css'
+import axios from 'axios'
+
+const ProductCard = () => {
+    return (
+        <div></div>
+    )
+}
+
+export default ProductCard
