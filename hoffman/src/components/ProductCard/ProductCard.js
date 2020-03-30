98baa71c2ee2d43dc08aeffe1f94b1f14c674148
Adam Hoffman <wintros@yahoo.com>
2020-03-30 17:34:38 -0600

    fonts?

--- hoffman/src/components/ProductCard/ProductCard.js
+++ hoffman/src/components/ProductCard/ProductCard.js
@@ -18,9 +18,12 @@ const ProductCard = props => {
     console.log("PRODUCT", product)
     return (
         <div className="container_card">
+            <h1>{product.name}</h1>
             <div className="inner_container_card">
             <img className="image_card" src={`http://res.cloudinary.com/hoffman-house/image/upload/${product.image}.jpg`} />
             <p className="card_text">Description: {product.description}</p>
+            <p className="card_text card_price">Price: ${product.price}</p>
+            <p className="card_text card_sku">SKU: {product.SKU}</p>
             </div>
         </div>
     )
